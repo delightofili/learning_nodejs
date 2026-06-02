30554a12d8d5c6777ea6a1302353fdf9e7ea0351
Delightsome <delightofili0@gmail.com>
2026-06-02 08:59:55 +0100

    wrapping it up with some assignments

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
 import http from "http";
-import fs from "fs";
 
-const server = http.createServer((req, res) => {
+import routes from "./route.js";
+
+const server = http.createServer(routes);
+
+server.listen(5000);
+
+const anotherServer = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
   if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
     res.write(
-      "<body><form action='/message' method='POST'><input name='message' type='text'/><button type='submit'>Send</button></form></body>",
+      "<html><title>Greetings</title><body><h1>Welcome Chinonso</h1></body></html>",
     );
-    res.write("</html>");
+    res.write(
+      "<body><p>you can change your name here</P><form action='/sub' method='POST'><input type='text' name='sub'/><button type='submit'>change</button></form></body>",
+    );
     return res.end();
   }
+  console.log("Another Server on Port 8000");
 
-  if (url === "/message" && method === "POST") {
+  if (url === "/sub" && method === "POST") {
     const body = [];
     req.on("data", (chunk) => {
-      console.log(chunk);
       body.push(chunk);
+      console.log(chunk);
     });
-    return req.on("end", () => {
+    req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
       const message = parsedBody.split("=")[1];
-      fs.writeFile("amess.txt", message, (err) => {
-        res.writeHead(302, { location: "/" }); //or same with res.statusCode = 302; res.setHeader("location", "/")
-        return res.end();
-      });
+      res.write("<html><body><h1>Welcome " + message + "</body></html>");
+      console.log(message);
+      return res.end();
     });
+    return;
   }
-
+  res.statusCode = 404;
   res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Nodejs Server</title></body>");
-  res.write("</html>");
+  res.write("<html><body.><h1>Page Not Found</h1></body.</html>");
   res.end();
 });
 
-server.listen(5000);
+anotherServer.listen(8000);
---
+++ route.js
@@ -0,0 +1,40 @@
+import fs from "fs";
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input name='message' type='text'/><button type='submit'>Send</button></form></body>",
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("amess.txt", message, (err) => {
+        res.writeHead(302, { location: "/" }); //or same with res.statusCode = 302; res.setHeader("location", "/")
+        return res.end();
+      });
+    });
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Nodejs Server</title></body>");
+  res.write("</html>");
+  res.end();
+};
+
+export default requestHandler;
