dd190a477471059c0c3274d5e4d07d3e21828489
Delightsome <delightofili0@gmail.com>
2026-06-03 18:21:20 +0100

    understood filtering paths

--- routes/admin.js
+++ routes/admin.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   res.send(
-    "<h1>Enter Product Details</h1><form action='/products' method='POST'><input type='text' name='title'/><button type='submit'>Add Product</button></form>",
+    "<h1>Enter Product Details</h1><form action='/admin/products' method='POST'><input type='text' name='title'/><button type='submit'>Add Product</button></form>",
   );
 });
 
-router.post("/product", (req, res, next) => {
+router.post("/products", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
