42ab7d635f75caedc049ac31f0d625301de5beea
Delightsome <delightofili0@gmail.com>
2026-06-03 17:21:08 +0100

    learnt about express, middleware, handling diffrent routes in express and also learnt about bodyParser,

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-import http from "http";
+//import http from "http";
+import express from "express";
+import bodyParser from "body-parser";
 
-import routes from "./route.js";
+const app = express();
 
-const server = http.createServer(routes);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-server.listen(3000);
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<h1>Enter Product Details</h1><form action='/products' method='POST'><input type='text' name='title'/><button type='submit'>Add Product</button></form>",
+  );
+});
+
+app.use("/products", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello Hello World!  (from Express)</h1>");
+});
+
+/* const server = http.createServer(app); */
+
+//server.listen(3000);
+
+app.listen(3000);
